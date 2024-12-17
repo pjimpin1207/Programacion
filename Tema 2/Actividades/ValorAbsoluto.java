3593b385d5830eaa2e50745197d3b11dd51ad20a
pjimpin1207 <pjimpin1207@g.educaand.es>
2024-12-17 11:23:07 +0100

    Delete Tema 2 directory

--- Tema 2/Actividades/ValorAbsoluto.java
+++
@@ -1,8 +0,0 @@
-package tema2;
-public class ValorAbsoluto {
-public static void main(String[] args) {
-    int x = -3;
-        int valorAbsoluto = Math.abs (x);
-        System.out.println(valorAbsoluto);
-}   
-}
--- Tema 2/ProyectoCalculadora/build/classes/proyectocalculadora/ProyectoCalculadora.rs
+++
@@ -1,1 +0,0 @@
-ProyectoCalculadora.ProyectoCalculadora
