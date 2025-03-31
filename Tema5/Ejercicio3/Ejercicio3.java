ba6d1150c7aaa5706801a4005b2f4a7789c36cda
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-03-31 12:32:11 +0200

    Update Ejercicio3.java

--- Tema5/Ejercicio3/Ejercicio3.java
+++ Tema5/Ejercicio3/Ejercicio3.java
@@ -1,4 +1,22 @@
 package Ejercicio3;
 
+import java.util.TreeSet;
+
 public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        TreeSet<Integer> conjunto = new TreeSet<>();
+
+        conjunto.add(50);
+        conjunto.add(40);
+        conjunto.add(90);
+        conjunto.add(80);
+
+        System.out.println(conjunto);
+
+        for (Integer i : conjunto) {
+            System.out.println(i);
+        }
+//
+    }
 }
