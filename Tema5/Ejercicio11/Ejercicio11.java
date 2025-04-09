97282a6ac1ec900deda3f0762337b18278fbb533
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-04-09 12:57:39 +0200

    Update Ejercicio11.java

--- Tema5/Ejercicio11/Ejercicio11.java
+++ Tema5/Ejercicio11/Ejercicio11.java
@@ -1,4 +1,31 @@
 package Ejercicio11;
 
+import java.util.LinkedList;
+
 public class Ejercicio11 {
+    public static void main(String[] args) {
+        // Crear la cola de tareas
+        LinkedList<String> tareas = new LinkedList<>();
+
+        // Añadir tareas a la cola
+        tareas.addLast("Lavar los platos");
+        tareas.addLast("Hacer la compra");
+        tareas.addLast("Estudiar Java");
+        tareas.addLast("Ir al gimnasio");
+        tareas.addLast("Leer un libro");
+
+        // Mostrar la cola de tareas
+        System.out.println("Cola de tareas:");
+        for (String tarea : tareas) {
+            System.out.println("- " + tarea);
+        }
+
+        // Mostrar tareas
+        public static void mostrarTareas (LinkedList < String > listaTareas) {
+            System.out.println("Tareas pendientes:");
+            for (String tarea : listaTareas) {
+                System.out.println("- " + tarea);
+            }
+        }
+    }
 }
